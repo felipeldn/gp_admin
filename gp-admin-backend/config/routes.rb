5526b174f0e1fdfa71146a80fd64e98ede39ca2a
felipeldn91 <felipeldn91@gmail.com>
2021-06-20 00:38:00 +0100

    started on sign in and validations

--- gp-admin-backend/config/routes.rb
+++ gp-admin-backend/config/routes.rb
@@ -1,23 +1,8 @@
 Rails.application.routes.draw do
-  resources :mindful_minutes
-  resources :falls
-  resources :heights
-  resources :weights
-  resources :exercise_durations
-  resources :flights_climbeds
-  resources :time_standings
-  resources :steps
-  resources :active_calories
-  resources :calorie_intakes
-  resources :blood_glucoses
-  resources :irreg_episodes
-  resources :temps
-  resources :o2_sats
-  resources :resp_rates
-  resources :blood_pressures
-  resources :heart_rates
-  resources :contacts
-  resources :clinicians
-  resources :patients
+  get '/clinicians', to: 'clinicians#index'
+  post '/signup', to: 'clinicians#signup'
+
+  get '/validate', to: 'auth#validate'
+  post 'signin', to: 'auth#signin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
